a3550456dafa051e86135e16852b5a6f15bc3852
umn678 <yulduzsuldan@gmail.com>
2025-07-26 00:56:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
 
 const app = express();
 
+// Google Search Console doğrulama dosyası
+app.get('/google123456.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'google0e31c5c7650e76aa.html'));
+});
+
+// Proxy yönlendirme
 app.use('/tumutoptan', createProxyMiddleware({
   target: 'https://thebravecartel.co.za',
   changeOrigin: true,
